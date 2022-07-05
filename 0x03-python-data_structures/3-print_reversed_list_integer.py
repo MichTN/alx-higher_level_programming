6d75a0c1e6a64d7e59ea5b53e67141f3ef747910
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-07-04 18:56:46 -0700

    Task 3 edits

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_new_list = my_list.copy()
-    for i in my_new_list.reverse():
-        print("{:d}.format(i))
+    for i in reversed(my_list):
+        print("{:d}".format(i))
