ad9a6c987b5b0b5a4f250f83c59307049655bfe1
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-07-05 14:19:41 -0700

    Task 7 edits

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,6 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a + (0, 0)
     if len(tuple_b) < 2:
         tuple_b = tuple_b + (0, 0)
-    l = tuple_a[0] + tuple_b[0]
-    m = tuple_a[1] + tuple_b[1]
-    return (l, m)
+    x = tuple_a[0] + tuple_b[0]
+    y = tuple_a[1] + tuple_b[1]
+    return (x, y)
