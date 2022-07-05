ced2126bafbfdd0f2f68c6a6b535ade601fe0fb0
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-07-04 19:28:07 -0700

    Task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,8 +2,8 @@
 def new_in_list(my_list, idx, element):
     my_list_two = my_list.copy()
     if idx < 0:
-        return my_list_two
+        return my_list
     if idx >= len(my_list_two):
-        return my_list_two
+        return my_list
     my_list_two[idx] = element:
         return my_list_two
