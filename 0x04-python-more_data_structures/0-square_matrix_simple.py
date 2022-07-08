cf7ec2b326a0eb130ff2b8590d21ed76f0b8f1d4
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-07-08 15:47:58 -0700

    squared integers

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/ython3
+def square_matrix_simple(matrix=[]):
+    matrix_new = []  
+    for i in matrix_new:
+        matrix_new.append(list(map(lambda x: x ** 2, i)))
+    return matrix
