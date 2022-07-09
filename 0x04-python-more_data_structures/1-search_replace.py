7122e2deedc39afc42e71c25df82175accbfe5e8
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-07-08 18:05:10 -0700

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    list_new = my_list[]
+    list_new = my_list[:]
     for x in range(len(list_new)):
         if list_new[x] == search:
             list_new[x] = replace
