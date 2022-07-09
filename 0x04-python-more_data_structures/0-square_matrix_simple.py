a4628f9c5e207af64b247594055071e11da85332
Tadiwa Nyamazana <nyamazana.tm@gmail.com>
2022-07-08 17:37:58 -0700

    integer squares

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     matrix_new = []  
-    for i in matrix_new:
+    for i in matrix:
         matrix_new.append(list(map(lambda x: x ** 2, i)))
     return matrix_new
